f7d4342bd199f69eae284c1e9cce9b0eabe9fe23
zoeebe05 <zoeebe05@gmail.com>
2024-08-27 08:04:53 -0400

    Add files via upload

---
+++ heroku.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+mkdir server
+python3 -m http.server -d server &
+
+# configure the bot
+python3 -m simplebot init "$ADDR" "$PASSWORD"
+python3 -m simplebot -a "$ADDR" set_name "$BOT_NAME"
+python3 -m simplebot -a "$ADDR" db -s "simplebot_instantview/max_size" "15728640"  # 15MB
+python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/mode" "command"
+python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/max_size" "1073741824"  # 1GB
+python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/delay" $DELAY
+python3 -m simplebot -a "$ADDR" db -s "simplebot_translator/filter_enabled" "no"
+#python3 ./restore_keys.py
+
+# add the web_comress plugin
+python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_compress.py'); open('web_compress.py', 'wb').write(r.content)"
+python3 -m simplebot -a "$ADDR" plugin --add ./web_compress.py
+
+# add the web_search plugin
+python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_search.py'); open('web_search.py', 'wb').write(r.content)"
+python3 -m simplebot -a "$ADDR" plugin --add ./web_search.py
+
+# add the youtube plugin
+python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/youtube.py'); open('youtube.py', 'wb').write(r.content)"
+python3 -m simplebot -a "$ADDR" plugin --add ./youtube.py
+
+# add the encryption_error plugin to leverage key changes
+python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/encryption_error.py'); open('encryption_error.py', 'wb').write(r.content)"
+python3 -m simplebot -a "$ADDR" plugin --add ./encryption_error.py
+
+# add admin plugin
+if [ -n "$ADMIN" ]; then
+    python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/admin.py'); open('admin.py', 'wb').write(r.content)"
+    python3 -m simplebot -a "$ADDR" plugin --add ./admin.py
+    python3 -m simplebot -a "$ADDR" admin --add "$ADMIN"
+fi
+
+# start the bot
+python3 -m simplebot -a "$ADDR" serve
